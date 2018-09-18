6cae9cbe68984aede585091e3a2949c17b552e86
Thirza Dado <tm.dado93@gmail.com>
2018-09-18 13:40:24 +0200

    GD cost function

--- GradientDescent.py
+++ GradientDescent.py
@@ -19,64 +19,32 @@ def sigmoid(a):
 # input x = [P N] (data matrix of P inputs for N examples), w = [1 P] (weight matrix)
 # output = [1 N] for N examples
 def forward(x, w):
-    # print "do forward"
     return sigmoid(linear(x, w))
 
 
 # loss function
 def cost(y, t, N):
+    # [y += 0.0001 for im in y for item in im if item == 0]
 
+    # for image in y:
+    #     # for item in image:
+    #     if image == 0:
+    #         y += 0.0001
 
-    for image in y:
-        for item in image:
-            if item == 0:
-                y += 0.0001
-    a = -t * np.log(y)
-    # print a
-
-    # print np.shape(t), np.shape(y)
-
-    b = (1 - t) * np.log(1 - y)
-    # print "a", a[0][:10]
-    # print "b", b[0][:10]
-    # print "a-b", (a - b)[0][:10]
-    print "a", a
-    print "b", b
-    print "a-b", (a - b)
-
-    # print np.shape(a), np.shape(b)
-    # print np.shape((a - b))
-
-    bb = a - b
-    # print bb
-
-    c = np.sum(bb)
-    print c
-    # print N
-    # print type(N)
-    # print 1/N * c
-    return 1/N * c
-
-    # return -1/N * np.sum(t * np.log(y) + (1 - t) * np.log(1 - y))
-
+    return -1/N * np.sum(t * np.log(y) + (1 - np.transpose(t)) * np.log(1 - y))
 
 # the gradient; backward propagation computes the backward pass for a one-layer network
 # with sigmoid units and loss
 def backward(x, y, t):
-    # print "do backward"
-
     return np.dot((y - np.transpose(t)), x)
-
-
-# Hessian
-# def hessian(x, y, N):
-#     return -1/N * np.sum()
 
 
 # load data
 x37_train, t37_train = load()
-x37_train = x37_train[:4]
-t37_train = t37_train[:4]
+x37_train = x37_train[:500]
+t37_train = t37_train[:500]
+x37_val = x37_train[501:600]
+t37_val = t37_train[501:600]
 
 # initialize weights
 w = np.random.randn(1, np.shape(x37_train)[1])
@@ -85,12 +53,15 @@ w = np.random.randn(1, np.shape(x37_train)[1])
 N = np.shape(x37_train)[0]
 
 # training
-epochs = 50
-eta = 0.001
-losses = []
+n_epochs = 70
+eta = 0.01
 xaxis = []
-for epoch in range(epochs):
-    print('hoi')
+
+# arrays for saving losses
+train_loss = np.zeros(n_epochs)
+val_loss = np.zeros(n_epochs)
+for epoch in range(n_epochs):
+
     # forward propagation
     y37_train = forward(np.transpose(x37_train), w)
 
@@ -101,12 +72,17 @@ for epoch in range(epochs):
     w = w - (eta * dW)
 
     # compute loss
-    losses.append(cost(y37_train, t37_train, N))
+    train_loss[epoch] = cost(y37_train, t37_train, N)
     xaxis.append(epoch)
+
+    y37_val = forward(np.transpose(x37_val), w)
+    val_loss[epoch] = cost(y37_val, t37_val, N)
 
 
 plt.figure(0)
-plt.plot(xaxis, losses)
+plt.plot(xaxis, train_loss)
+plt.plot(xaxis, val_loss)
+plt.legend(["training set", "validation set"])
 plt.show()
 
 
