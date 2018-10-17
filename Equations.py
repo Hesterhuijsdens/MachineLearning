706c8d1fdf04d280d9097d04e8368cf2f6177a68
Hesterhuijsdens <hesterhuijsdens@gmail.com>
2018-10-17 10:29:54 +0200

    Hessian matrix

--- Equations.py
+++ Equations.py
@@ -8,7 +8,7 @@ def linear(x, w):
 
 # sigmoid
 def sigmoid(a):
-    return 1 / (1 + np.exp(-a.astype(float)))
+    return 1.0 / (1 + np.exp(-a.astype(float)))
 
 
 # forward propagation
@@ -31,11 +31,11 @@ def cost_decay(y, t, decay, w):
     N = np.shape(t)[0]
     y[y < 0.001] = 0.001
     y[y > 0.999] = 0.999
-    cost = (-1.0 / N) * np.sum(t * np.log(y) + (1 - np.transpose(t)) * np.log(1 - y))
+    cost_without = (-1.0 / N) * np.sum(t * np.log(y) + (1 - np.transpose(t)) * np.log(1 - y))
     sum = 0
-    for n in range(1, N):
+    for n in range(1, N + 1):
         sum += (decay/(2*n)) * np.dot(w[0, 0:n], w[0, 0:n])
-    return cost - (1.0/N) * sum
+    return cost_without - (1.0/N) * sum
 
 
 # the gradient; backward propagation computes the backward pass for a one-layer network
@@ -47,27 +47,25 @@ def backward(x, y, t):
 # the gradient of E with weight decay:
 def gradient_e_decay(y, t, x, decay, w):
     N = np.shape(x)[0]
-    sum = 0
-    for n in range(1, N):
-        sum += (decay/n) * w
-    return (1.0/N) * np.matmul(y - np.transpose(t), x) + np.mean(sum, 0)
+    gradient = np.zeros(np.shape(w)[1])
+    for i in range(np.shape(w)[1]):
+        formula_inside = (y - t) * x[:, i] + np.reshape((decay / np.arange(1, N + 1)) * w[0, i], (1, N))
+        gradient[i] = 1.0 / N * np.sum(formula_inside)
+    return gradient
 
 
 # computes the Hessian with weight decay:
-def hessian(x, y, decay, d):
+def hessian(x, y, decay):
     N = np.shape(x)[0]
-    H = np.ones((d, d))
-    for i in range(d):
-        for j in range(d):
-            ans = np.multiply(np.multiply(np.multiply(x[:,i], y), 1-y), x[:,j])
-            if i == j:
-                for n in range(1, N): # weight decay
-                    ans += (decay/n)
-            H[i, j] = np.sum(ans)
-    return (1.0/N) * H
+    d = np.shape(x)[1]
+    formula_inside = 0
+    for n in range(N):
+        formula_inside += np.matmul(np.transpose(np.reshape(x[n, :], (1, d)) * y[0, n] * (1 - y[0, n])),
+                                    np.reshape(x[n, :], (1, d)))
+    return (1.0 / N) * (formula_inside + np.sum(decay / np.arange(1, N + 1)) * np.identity(d))
 
 
-# Computes the percentage of misclassified patterns (for NewtonMethod):
+# computes the percentage of misclassified patterns (for NewtonMethod):
 def classification_error(y, t):
     mistakes = 0
     for i in range(np.shape(y)[1]):
