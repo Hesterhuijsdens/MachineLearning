0fb3c7a8f28cc082fba9f418b0b4fde0e05f47f4
Hesterhuijsdens <hesterhuijsdens@gmail.com>
2018-10-17 12:01:57 +0200

    Newton Method with momentum

--- NewtonMethod.py
+++ NewtonMethod.py
@@ -4,20 +4,21 @@ from PreprocessData import load
 from Equations import *
 import time
 
-x, t = load(version="train")   # Load the data
+# load train and test data:
+x, t = load(version="train")
 x = x[:300]
 t = t[:300]
-print "t: ", t
 x_test, t_test = load(version="test")
-#x_test = x_test[:500]
-#t_test = t_test[:500]
+x_test = x_test[:100]
+t_test = t_test[:100]
 
+# store dimensions of data:
 N = np.shape(x)[0]
 d = np.shape(x)[1]
 w = np.random.rand(1, d)
 
-# lambda
-decay = 0.1
+# set parameters:
+decay = 0.01
 epochs = 10
 losses = []
 losses_test = []
@@ -26,18 +27,26 @@ xaxis = []
 # Start time:
 start = time.time()
 
+dW = 0
 for epoch in range(epochs):
-    print("Epoch: ", epoch)
+    print "Epoch: ", epoch
+
+    # forward computation and losses:
     y = forward(np.transpose(x), w)
-    print "y: ", y
-    losses.append(cost(y, t))#cost_decay(y, t, decay, w))
-    losses_test.append(cost_decay(forward(np.transpose(x_test), w), t_test, decay, w))
+    losses.append(cost(y, t))
+    y_test = forward(np.transpose(x_test), w)
+    losses_test.append(cost(y_test, t_test))
     xaxis.append(epoch)
 
+    # compute gradient and hessian and update the weights:
     grE = gradient_e_decay(y, t, x, decay, w)
     H = hessian(x, y, decay)
-    H_inv = np.linalg.inv(H.astype(float))
-    w = w - np.transpose(np.matmul(H_inv, np.transpose(grE)))
+    H_inv = np.linalg.pinv(H.astype(float))
+    H_grE = np.transpose(np.matmul(H_inv, np.transpose(grE)))
+
+    # use momentum to update weights
+    dW = -0.05 * H_grE + 0.9 * dW
+    w = w + dW
 
 
 # Compute and plot results:
