1991f8fdb42d92e910419604ba8c24e63cc2ea2b
Hesterhuijsdens <hesterhuijsdens@gmail.com>
2019-01-23 10:38:27 +0100

    Opimal control sequence

--- Assignment6/controlled_random_walk.py
+++ Assignment6/controlled_random_walk.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 # initialize parameters:
 T = 20.0
 dt = 0.01
-v = 0.5
+v = 0.01
 trials = 20
 
 # random walk:
@@ -17,11 +17,26 @@ for n in range(trials):
     # visualize random walk:
     plt.plot(range(int(T / dt)), x)
 
+# optimal sequence of actions:
+x_optimal = [0.0]
+u_optimal = []
+T = T / dt
+dt = 1.0
+for t in range(int(T / dt)-1):
+    # compute optimal control:
+    u_optimal.append((1.0 / (T - t)) * (np.tanh(x_optimal[-1] / (v * (T - t))) - x_optimal[-1]))
+
+    # take optimal action u:
+    x_optimal.append(x_optimal[-1] + u_optimal[-1] + np.random.normal(loc=0, scale=v * dt))
+    print x_optimal[-1]
+
+plt.plot(range(int(T / dt)), x_optimal, linewidth=3, color='black')
+
 # add title and labels:
 plt.title('Controlled random walk')
 plt.xlabel('t')
 plt.ylabel('x')
 locks, labels = plt.xticks()
-plt.xticks(locks, locks*dt)
+plt.xticks(locks, locks*0.01)
 plt.xlim([0.0, 2000.0])
 plt.show()
