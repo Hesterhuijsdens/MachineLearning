bf8340d6299e0cdda71723ad34e73a74d06f9cae
Hesterhuijsdens <hesterhuijsdens@gmail.com>
2019-01-29 12:49:44 +0100

    Newton method (final)

--- Assignment1/NewtonMethod.py
+++ Assignment1/NewtonMethod.py
@@ -6,11 +6,7 @@ import time
 
 # load train and test data:
 x, t = load37(version="train")
-#x = x[:300]
-#t = t[:300]
 x_test, t_test = load37(version="test")
-#x_test = x_test[:100]
-#t_test = t_test[:100]
 
 # store dimensions of data:
 N = np.shape(x)[0]
@@ -21,7 +17,6 @@ w = np.random.randn(1, d)
 decay = 0.1
 epochs = 10
 losses = []
-losses_test = []
 xaxis = []
 
 # Start time:
@@ -33,9 +28,6 @@ for epoch in range(epochs):
     # forward computation and losses:
     y = forward(np.transpose(x), w)
     losses.append(cost_decay(y, t, decay, w))
-
-    y_test = forward(np.transpose(x_test), w)
-    losses_test.append(cost_decay(y_test, t_test, decay, w))
     xaxis.append(epoch)
 
     # compute gradient and hessian and update the weights:
@@ -44,24 +36,31 @@ for epoch in range(epochs):
     H_inv = np.linalg.pinv(H.astype(float))
     w = w - np.matmul(H_inv, np.transpose(grE))
 
-# compute and plot results:
+
+# compute test results:
+y_test = forward(np.transpose(x_test), w)
+loss_test = cost_decay(y_test, t_test, decay, w)
+
+# plot and print results:
 class_err = classification_error(y, t)
 print "class_err: ", class_err
-print "E: ", cost_decay(y, t, decay, w)
+print " E (decay): ", cost_decay(y, t, decay, w)
+print " E: ", cost(y, t)
 
 class_err_test = classification_error(forward(np.transpose(x_test), w), t_test)
 print "class_err_test: ", class_err_test
-print "E test: ", cost_decay(forward(np.transpose(x_test), w), t_test, decay, w)
+print "E test (decay): ", cost_decay(forward(np.transpose(x_test), w), t_test, decay, w)
+print "E test: ", cost(forward(np.transpose(x_test), w), t_test)
 
 # stop time:
 end = time.time()
 print "time: ", end - start
 
 plt.figure(1)
-plt.plot(xaxis, losses, label='train')
-plt.plot(xaxis, losses_test, label='test')
+plt.plot(xaxis, losses, label='train', color='royalblue')
+plt.plot(xaxis, np.repeat(loss_test, epochs), label='test', color='grey', linestyle='--')
 plt.xlabel('number of epochs')
 plt.ylabel('loss')
-plt.title('Loss for Newton method with weight decay of ' + str(decay))
+plt.title('Newton method with weight decay of ' + str(decay))
 plt.legend()
 plt.show()
