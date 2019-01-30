a6ba9b53212274733d262680e623156677e2dbf3
Hesterhuijsdens <hesterhuijsdens@gmail.com>
2019-01-30 20:49:37 +0100

    lasso coefficients to zero (final)

--- Assignment3/lasso.py
+++ Assignment3/lasso.py
@@ -1,14 +1,15 @@
 import numpy as np
-from load import loadLasso, normalize2
+from load import loadLasso, normalize
 import matplotlib.pyplot as plt
 from equations_regression import *
-from sklearn.linear_model import Ridge
+
+np.random.seed(40)
 
 # load lasso data and normalize:
 X, y = loadLasso(version="train")   # (50, 100) & (50,)
 Xv, yv = loadLasso(version="test")
-X = normalize2(X)
-Xv = normalize2(Xv)
+X = normalize(X)
+Xv = normalize(Xv)
 
 # add bias term (column of ones):
 x = np.ones((np.shape(X)[0], np.shape(X)[1] + 1))
@@ -22,43 +23,47 @@ n = np.shape(x)[1] # dimensions -> 101
 p_test = np.shape(xv)[0]
 
 # set regularization term:
-decay = 0.09
+decay = 0.01#0.000000000000000000001 #0.018 #0.01
 
 # initialization for LASSO:
-max_iteration = 50
-cost = np.zeros(max_iteration)
+cost = []
 w = np.random.normal(loc=0, scale=0.1, size=n)
-w_plot = np.zeros((n, max_iteration))
-cost_test = np.zeros(max_iteration)
+w_plot = np.zeros((n, 1000))
+cost_test = []
 
 # coordinate descent:
-for epoch in range(max_iteration):
+epoch = 0
+converged = False
+
+while not converged or epoch < 10:
     # save w for plotting:
+    print epoch
     w_plot[:, epoch] = w
+
+    converged = True
 
     for i in range(1, n): # for all dimensions (100)
         # compute gradient of one single feature i and update:
         ytilde = y - np.matmul(w, np.transpose(x)) + w[i] * x[:, i]
         btilde = (1.0/p) * np.dot(ytilde, x[:, i])
-        w[i] = soft_threshold(btilde, decay) #/(x[:, i]**2).sum()
+        w_new = soft_threshold(btilde, decay) #/(x[:, i]**2).sum()
 
-    cost[epoch] = cost_function(p, w, x, y, decay)
-    cost_test[epoch] = cost_function(p_test, w, xv, yv, decay)
+        if np.abs(w_new - w[i]) >= 0.000001:
+            converged = False
+        w[i] = 0.8 * w_new + 0.2 * w[i]
 
-print "cost lasso (training): ", cost_function(p, w, x, y, decay)
+    cost.append(cost_function(p, w, x, y, decay))
+    cost_test.append(cost_function(p_test, w, xv, yv, decay))
+    epoch += 1
 
-# print "cost ridge: ", cost_function(p, w_ridge, x, y, decay)
-# ridge = Ridge(alpha=1.0, fit_intercept=False).fit(x, y)
-# print "cost 2 ridge: ", cost_function(p, ridge.coef_, x, y, decay)
-
-# plot the costs of lasso as function of epoch:
-plt.figure(1)
-plt.plot(range(max_iteration), cost, label="train")
-plt.plot(range(max_iteration), cost_test, label="test")
-plt.legend()
+print "epochs: ", epoch
 
 # plot w as function of epoch:
-plt.figure(2)
-plt.plot(range(max_iteration), np.transpose(w_plot))
+plt.figure(1)
+#plt.plot(range(epoch), np.transpose(np.transpose(w_plot)[1:101]))
+plt.plot(range(epoch), np.transpose(w_plot[1:101, 0:epoch]))
+plt.xlabel('iteration')
+plt.ylabel('coefficients w')
+plt.title('LASSO using coordinate descent')
 plt.show()
 
